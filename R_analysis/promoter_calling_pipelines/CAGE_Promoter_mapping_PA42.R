b0777930a6525127e08bacd918d214f42c3887d6
Taylor Raborn <rtraborn@indiana.edu>
2015-05-29 21:52:37 -0400

    Updated PA42 promoter mapping pipeline

--- R_analysis/promoter_calling_pipelines/CAGE_Promoter_mapping_PA42.R
+++ R_analysis/promoter_calling_pipelines/CAGE_Promoter_mapping_PA42.R
@@ -6,14 +6,14 @@ require(CAGEr)
 #load the genome as appropriate
 require(BSgenome.Dpulex.PA42)
 #enter the location of the directory of mapped bam files from CAGE or other 5' end reads. (Note: the directory must ONLY have the bam files of interest in it)
-thisDir <- c("/home/rtraborn/Daphnia/CAGE/demultiplexed_matched/bam_files/PA42_bams/filtered/merged/")
+thisDir <- c("/home/rtraborn/Daphnia/CAGE/demultiplexed_matched/bam_files/PA42_bams/filtered/replicate_bams")
 pathsToInputFiles <- list.files(thisDir, full.names = TRUE)   
 
 #creates the CAGEset S4 object. Enter the genomeName and sampleLabels as appropriate for your analysis
-myCAGEset <- new("CAGEset",genomeName="BSgenome.Dpulex.PA42",inputFiles=pathsToInputFiles,inputFilesType="bam",sampleLabels=c("mature_females","mature_males","pE_females"))
+myCAGEset <- new("CAGEset",genomeName="BSgenome.Dpulex.PA42",inputFiles=pathsToInputFiles,inputFilesType="bam",sampleLabels=c("mat_females_1","mat_females_2","mat_females_3","mat_males_1","mat_males_2","pE_females_1","pE_females_2","pE_females_3"))
 
 #calculates and plots the pearson correlation between all samples
-corr.m <- plotCorrelation(myCAGEset, samples = "all", method = "pearson")
+#corr.m <- plotCorrelation(myCAGEset, samples = "all", method = "pearson")
 
 #calculating the TSS clusters (a subunit of TSRs)
 getCTSS(myCAGEset)
@@ -27,7 +27,7 @@ cat("The number of CTSS in sample is", nCTSS,"\n")
 plotReverseCumulatives(myCAGEset, fitInRange = c(5, 1000), onePlot = TRUE)
 normalizeTagCount(myCAGEset, method = "powerLaw", fitInRange = c(5, 1000), alpha = 1, T = 1*10^6)
 #uncomment the following line if you want a bedgraph file of the CTSSs in the sample
-exportCTSStoBedGraph(myCAGEset, values = "normalized", oneFile = TRUE)
+#exportCTSStoBedGraph(myCAGEset, values = "normalized", oneFile = TRUE)
 
 #saves an R binary of the data. 
 save.image("Dp_PA42.RData")
@@ -39,11 +39,6 @@ clusterCTSS(object = myCAGEset, threshold = 1, thresholdIsTpm = TRUE,nrPassThres
 cumulativeCTSSdistribution(myCAGEset, clusters = "tagClusters")
 quantilePositions(myCAGEset, clusters = "tagClusters", qLow = 0.1, qUp = 0.9)
 
-#creates objects containing TCs with interquantile widths and writes the summary statistics to a table
-asex_TC <- tagClusters(myCAGEset, sample = "asexual_females", returnInterquantileWidth = TRUE, qLow=0.1, qUp=0.9)
-pE_TC <- tagClusters(myCAGEset, sample = "pE_females", returnInterquantileWidth = TRUE, qLow=0.1, qUp=0.9)
-male_TC <- tagClusters(myCAGEset, sample = "sexual_males", returnInterquantileWidth = TRUE, qLow=0.1, qUp=0.9)             
-
 #aggregates tag clusters and then calculates the consensus clusters (promoters across all three conditions) within the sample
 aggregateTagClusters(myCAGEset, tpmThreshold = 5, qLow = 0.1, qUp = 0.9, maxDist = 100)
 quantilePositions(myCAGEset, clusters="consensusClusters", returnInterquartileWidth=TRUE,useMulticore=TRUE)
@@ -51,7 +46,6 @@ consCl <- consensusClusters(myCAGEset)
 
 TSR_summary <- summary(consCl)
 write.table(TSR_summary,file="TSR_interquantile_summary_PA42.txt",sep=" ",col.names=TRUE,row.names=FALSE)
-
 
 #exports a bed file of the TSRs' interquantile widths
 exportToBed(myCAGEset, what = "consensusClusters", colorByExpressionProfile = TRUE)
