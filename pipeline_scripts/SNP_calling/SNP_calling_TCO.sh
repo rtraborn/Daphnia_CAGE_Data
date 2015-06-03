5bc5a15de9964ad174dbe8a16127a3309fc8a6c9
Taylor Raborn <rtraborn@indiana.edu>
2015-06-03 12:50:24 -0400

    Added a SNP calling pipeline and other misc items

---
+++ pipeline_scripts/SNP_calling/SNP_calling_TCO.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WD=/home/rtraborn/Daphnia/CAGE/demultiplexed_matched/bam_files/TCO_bams/bam_filtered/files/
+
+cd $WD
+
+echo "Merging the alignment replicate files, as appropriate"
+
+echo "Merging mature females"
+
+samtools merge -r -@ 4 Dp_mat_females_filtered.bam Daphnia_mat_females_1.rRNA.bam Daphnia_mat_females_2.rRNA.bam Daphnia_mat_females_3.rRNA.bam
+
+echo "Merging pE females"
+
+samtools merge -r -@ 4 Dp_pE_females_filtered.bam Daphnia_pE_females_1.rRNA.bam Daphnia_pE_females_2.rRNA.bam Daphnia_pE_females_3.rRNA.bam
+
+echo "Merging mature males"
+
+samtools merge -r -@ 4 Dp_mat_males_filtered.bam Daphnia_mature_males_1.rRNA.bam Daphnia_mature_males_2.rRNA.bam
+
+echo "Calling SNPs from each merged alignment"
+
+for BM in *.filtered.bam
+do
+  samtools mpileup -I -v --output $(basename $BM .filtered.bam).vcf  
+
+done
+
+echo "Job Complete!"
+
