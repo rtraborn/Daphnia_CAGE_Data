a71c37df5bd010175e8ef5d7b9f0390a96a8f358
Taylor Raborn <rtraborn@indiana.edu>
2015-06-08 17:28:19 -0400

    Updated heatmaps using heatmap.2 cont6

--- R_analysis/expression_analysis/de_analysis_TCO.R
+++ R_analysis/expression_analysis/de_analysis_TCO.R
@@ -301,7 +301,7 @@ par(mar=c(4.1,4.1,4.1,4.1))
 png(file="heatmap_TCO_upreg1.png",height=2200,width=2400)
 selected  <- rownames(top_table_e[1:200])
 esetSel <- dp_eset[selected, ]
-heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="none",
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="row",
           key=TRUE,)
 dev.off()
 
@@ -310,7 +310,7 @@ par(mar=c(4.1,4.1,4.1,4.1))
 png(file="heatmap_TCO_upreg2.png",height=2200,width=2400)
 selected  <- rownames(top_table_e2[1:200])
 esetSel <- dp_eset[selected, ]
-heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="none",
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="row",
           key=TRUE,)
 dev.off()
 
@@ -319,6 +319,6 @@ par(mar=c(4.1,4.1,4.1,4.1))
 png(file="heatmap_TCO_upreg3.png",height=2200,width=2400)
 selected  <- rownames(top_table_e3[1:200])
 esetSel <- dp_eset[selected, ]
-heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="none",
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,symbreaks=TRUE, scale="row",
           key=TRUE,)
 dev.off()
