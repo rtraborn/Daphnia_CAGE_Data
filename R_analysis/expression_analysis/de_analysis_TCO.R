1114b1d8b55feeb1c0ba84856387ac4cd3f64528
Taylor Raborn <rtraborn@indiana.edu>
2015-06-14 18:07:48 -0400

    Converted edgeR to limma, which is more stringent. Need to fix heatmaps, most of which are broken by this code

--- R_analysis/expression_analysis/de_analysis_TCO.R
+++ R_analysis/expression_analysis/de_analysis_TCO.R
@@ -68,14 +68,6 @@ Dp_dge <- Dp_dge[A>rowsum_threshold,]
 Dp_dge <- estimateCommonDisp(Dp_dge, verbose=T)
 Dp_dge <- estimateTagwiseDisp(Dp_dge, trend="none")
 
-#gfit <- glmFit(Dp_dge, design)
-#lrt_pE_male <- glmLRT(gfit, contrast=pE_v_male)
-#lrt_male_mf <- glmLRT(gfit, contrast=male_v_matfem)
-#lrt_matfem_v_pE <- glmLRT(gfit, contrast=matfem_v_pE)
-#lrt_male_v_fem <- glmLRT(gfit, contrast=male_v_females)
-#lrt_asex_v_sex <- glmLRT(gfit, contrast=asex_v_sexuals)
-#head(lrt)
-
 plotBCV(Dp_dge)
 
 Dp_dge <- calcNormFactors(Dp_dge)
@@ -90,170 +82,38 @@ length(ind)
 
 head(res)
 
-vennDiagram(res)
-
 options(digits=3)
-de_table1 <- topTable(fit2, coef=1, sort.by="logFC",adjust.method="BH")
+de_table1 <- topTable(fit2, coef=1, adjust.method="BH",n=Inf)
 head(de_table1)
-de_table2 <- topTable(fit2, coef=2, sort.by="logFC",adjust.method="BH")
+de_table2 <- topTable(fit2, coef=2, adjust.method="BH",n=Inf)
 head(de_table2)
-de_table3 <- topTable(fit2, coef=3, sort.by="logFC",adjust.method="BH")
+de_table3 <- topTable(fit2, coef=3, adjust.method="BH",n=Inf)
 head(de_table3)
-de_table4 <- topTable(fit2, coef=4, sort.by="logFC",adjust.method="BH")
-de_table5 <- topTable(fit2, coef=5, sort.by="logFC",adjust.method="BH")
+de_table4 <- topTable(fit2, coef=4, adjust.method="BH",n=Inf)
+de_table5 <- topTable(fit2, coef=5, adjust.method="BH",n=Inf)
 
 plotMDS(v, labels=c("mf1","mf2","mf3","m1","m2","pE1","pE2","pE3"), main="MDS plot for all eight libraries")
-volcanoplot(fit2,coef=1)
-volcanoplot(fit2,coef=2)
-volcanoplot(fit2,coef=3)
+volcanoplot(fit2,coef=1,highlight=20)
+volcanoplot(fit2,coef=2,highlight=20)
+volcanoplot(fit2,coef=3,highlight=20)
+volcanoplot(fit2,coef=4,highlight=20)
+volcanoplot(fit2,coef=5,highlight=20)
 
-###### male vs pE females #######################
-et <- exactTest(Dp_dge,pair=c("mat_male","pE_fem"))
-topTags(et, n=20)
-de_pE_male <- decideTestsDGE(et, adjust="BH",p.value=0.01)
-summary(de_pE_male)
-detags <- rownames(Dp_dge)[as.logical(de_pE_male)]
-de.genes_pE_male <- rownames(Dp_dge)[as.logical(de_pE_male)]
-plotSmear(Dp_dge, de.tags = de.genes_pE_male, cex = 0.5)
-abline(h = c(-2, 2), col = "blue")
-
-top_table_e <- topTags(et, sort.by="PValue",n=Inf)
-top_table_e$FDR <- p.adjust(top_table_e$PValue, method = "BH")
-dim(top_table_e)
-
-data_coord2 <- matrix(data=unlist(strsplit(rownames(top_table_e), split="_")),
-                      nrow= length(row.names(top_table_e)),
-                      byrow=T)
-
-data_coord2 <- as.data.frame(data_coord2, stringsAsFactors=F)
-head(data_coord2)
-
-col_1 <- data_coord2[,1]
-col_2 <- data_coord2[,2]
-chr_col <- paste(col_1,col_2,sep="_")
-data_coord2 <- data_coord2[,-2]
-data_coord2[,1] <- chr_col
-data_coord2[,2] <- as.numeric(data_coord2[,2])
-data_coord2[,3] <- as.numeric(data_coord2[,3])
-names(data_coord2) <- c('chr','start','end','strand')
-
-#first, creating a genomicRanges object from the promoter data
-de_GR_1 <- with(data_coord2, GRanges(chr,
-                                    IRanges(start, end, names=row.names(top_table_e)), strand))
-
-
-de_GR_1 <- promoters(de_GR_1, upstream=200, downstream=200)
-
-#what does the GR object look like?
-de_GR_1
-
-options(digits=3)
-
-###### pE female vs mature females #######################
-et2 <- exactTest(Dp_dge,pair=c("pE_fem","mat_fem"))
-topTags(et2, n=20)
-de_pE_matfem <- decideTestsDGE(et2, adjust="BH",p.value=0.01)
-summary(de_pE_matfem)
-detags <- rownames(Dp_dge)[as.logical(de_pE_matfem)]
-de.genes_pE_matfem <- rownames(Dp_dge)[as.logical(de_pE_matfem)]
-plotSmear(Dp_dge, de.tags = de.genes_pE_matfem, cex = 0.5)
-abline(h = c(-2, 2), col = "blue")
-
-top_table_e2 <- topTags(et2, sort.by="PValue",n=Inf)
-top_table_e2$FDR <- p.adjust(top_table_e2$PValue, method = "BH")
-
-data_coord2 <- matrix(data=unlist(strsplit(rownames(top_table_e2), split="_")),
-                      nrow= length(row.names(top_table_e2)),
-                      byrow=T)
-data_coord2 <- as.data.frame(data_coord2, stringsAsFactors=F)
-head(data_coord2)
-
-col_1 <- data_coord2[,1]
-col_2 <- data_coord2[,2]
-chr_col <- paste(col_1,col_2,sep="_")
-data_coord2 <- data_coord2[,-2]
-data_coord2[,1] <- chr_col
-data_coord2[,2] <- as.numeric(data_coord2[,2])
-data_coord2[,3] <- as.numeric(data_coord2[,3])
-names(data_coord2) <- c('chr','start','end','strand')
-
-#first, creating a genomicRanges object from the promoter data
-de_GR_2 <- with(data_coord2, GRanges(chr,
-                                    IRanges(start, end, names=row.names(top_table_e2)), strand))
-
-de_GR_2 <- promoters(de_GR_2, upstream=200, downstream=200)
-
-#what does the GR object look like?
-de_GR_2
-
-###### asexual females vs males  #######################
-et3 <- exactTest(Dp_dge,pair=c("mat_fem","mat_male"))
-topTags(et3, n=20)
-de_matfem_male <- decideTestsDGE(et3, adjust="BH",p.value=0.01)
-summary(de_matfem_male)
-detags <- rownames(Dp_dge)[as.logical(de_matfem_male)]
-de.genes_matfem_male <- rownames(Dp_dge)[as.logical(de_matfem_male)]
-plotSmear(Dp_dge, de.tags = de.genes_matfem_male, cex = 0.5)
-abline(h = c(-2, 2), col = "blue")
-
-top_table_e3 <- topTags(et3, sort.by="PValue",n=Inf)
-top_table_e3$FDR <- p.adjust(top_table_e3$PValue, method = "BH")
-
-data_coord2 <- matrix(data=unlist(strsplit(rownames(top_table_e3), split="_")),
-                      nrow= length(row.names(top_table_e3)),
-                      byrow=T)
-
-data_coord2 <- as.data.frame(data_coord2, stringsAsFactors=F)
-head(data_coord2)
-
-col_1 <- data_coord2[,1]
-col_2 <- data_coord2[,2]
-chr_col <- paste(col_1,col_2,sep="_")
-data_coord2 <- data_coord2[,-2]
-data_coord2[,1] <- chr_col
-data_coord2[,2] <- as.numeric(data_coord2[,2])
-data_coord2[,3] <- as.numeric(data_coord2[,3])
-names(data_coord2) <- c('chr','start','end','strand')
-
-#first, creating a genomicRanges object from the promoter data
-de_GR_3 <- with(data_coord2, GRanges(chr,
-                                    IRanges(start, end, names=row.names(top_table_e3)), strand))
-
-
-de_GR_3 <- promoters(de_GR_3, upstream=200, downstream=200)
-
-#what does the GR object look like?
-de_GR_3
-
-#removing the limma commands for now
-v <- voom(Dp_dge,design,plot=TRUE)
-fit <- lmFit(v,design=design)
-head(fit)
-fit2 <- eBayes(fit)
-
-topTable(fit2,coef=ncol(design))
-
-top_table <- topTable(fit2,coef=ncol(design),n=Inf,sort.by="p",adjust="BH",p=0.01)
-results <- decideTests(fit2)
-
-vennDiagram(results, names=c("Asexual females","Sexual females","Sexual Males"),include="up",circle.col=c("green","red","blue"))
+#results <- decideTests(fit2)
+#vennDiagram(results, names=c("Asexual females","Sexual females","Sexual Males"),include="up",circle.col=c("green","red","blue"))
 
 #top_table2 <- cbind(top_table,results)
 #write.table(top_table2,file="de_TCO_combined_table.txt", row.names=TRUE,quote=FALSE)
 
-head(et)
-
-head(top_table_e)
-
-################################### male vs pE females ############################
+################################### mat females vs pE females ############################
 
 de_data <- Dp_dge$pseudo.counts
 
 #differential analysis results
-de_data <- cbind(de_data, et$table)
+de_data <- cbind(de_data, de_table1)
 
 #calculating the false discovery rate
-de_data$FDR <- p.adjust(de_data$PValue, method = 'BH')
+de_data$FDR <- p.adjust(de_data$P.Value, method = 'BH')
 
 #dispersion of each tag cluster
 de_data$tw_dis <- Dp_dge$tagwise.dispersion
@@ -285,17 +145,16 @@ de_data$end <- as.numeric(de_data$end)
 
 de_data1 <- de_data
 
-#write.table(de_data1,file="TCO_pE_v_male_de.txt",col.names=TRUE,quote=FALSE)
+write.table(de_data1,file="TCO_male_v_mat_fem_de.txt",col.names=TRUE,quote=FALSE)
 
-################################### pE females vs asexual females ############################
+################################### pE females vs males ############################
 
 de_data <- Dp_dge$pseudo.counts
 
 #differential analysis results
-de_data <- cbind(de_data, et2$table)
+de_data <- cbind(de_data, de_table2)
 
-#calculating the FDR
-de_data$FDR <- p.adjust(de_data$PValue, method = 'BH')
+de_data$FDR <- p.adjust(de_data$P.Value, method = 'BH')
 
 #dispersion of each tag cluster
 de_data$tw_dis <- Dp_dge$tagwise.dispersion
@@ -327,17 +186,17 @@ de_data$end <- as.numeric(de_data$end)
 
 de_data2 <- de_data
 
-#write.table(de_data2,file="TCO_pE_v_matfem_de.txt",col.names=TRUE,quote=FALSE)
+write.table(de_data2,file="TCO_pE_v_males_de.txt",col.names=TRUE,quote=FALSE)
 
-###################################  asexual females vs males  ############################
+###################################  asexual females vs pE_fem  ############################
 
 de_data <- Dp_dge$pseudo.counts
 
 #differential analysis results
-de_data <- cbind(de_data, et3$table)
+de_data <- cbind(de_data, de_table3)
 
 #calculating the FDR
-de_data$FDR <- p.adjust(de_data$PValue, method = 'BH')
+de_data$FDR <- p.adjust(de_data$P.Value, method = 'BH')
 
 #dispersion of each tag cluster
 de_data$tw_dis <- Dp_dge$tagwise.dispersion
@@ -370,7 +229,7 @@ de_data$end <- as.numeric(de_data$end)
 
 de_data3 <- de_data
 
-#write.table(de_data3,file="TCO_matfem_v_males_de.txt",col.names=TRUE,quote=FALSE)
+write.table(de_data3,file="TCO_matfem_v_pE_de.txt",col.names=TRUE,quote=FALSE)
 
 ####################################
 #Adding gene annotation to promoters
@@ -555,53 +414,53 @@ gustatory_de <- gustatory_table[de_index,]
 #overall heatmap
 par(mar=c(4.1,4.1,4.1,4.1))
 png(file="all_genes_heatmap.png",height=2800,width=2800)
-selected  <- rownames(top_table_e)
+selected  <- rownames(de_table1)
 esetSel <- dp_eset[selected,]
-#heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
-#          key=TRUE,margins=c(10,10))
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
+          key=TRUE,margins=c(10,10))
 dev.off()
 
-#male vs pE
-par(mar=c(4.1,4.1,4.1,4.1))
-png(file="heatmap_TCO_pE_v_male.png",height=2800,width=2800)
-selected  <- rownames(top_table_e[1:200])
-is(selected)
-esetSel <- dp_eset[selected, ]
-#heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
-#          key=TRUE,margins=c(10,10))
-#dev.off()
-
-#mat_fem vs pE
-par(mar=c(4.1,4.1,4.1,4.1))
-png(file="heatmap_TCO_matfem_v_pE.png",height=2800,width=2800)
-selected  <- rownames(top_table_e2[1:200])
-esetSel <- dp_eset[selected, ]
-#heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
-#          key=TRUE,margins=c(10,10))
-#dev.off()
-
-#male vs mat_fem
+#male vs asexuals
 par(mar=c(4.1,4.1,4.1,4.1))
 png(file="heatmap_TCO_male_v_matfem.png",height=2800,width=2800)
-selected  <- rownames(top_table_e3[1:200])
+selected  <- rownames(de_table1[de_table1$FDR<0.01,])
+is(selected)
+esetSel <- dp_eset[selected, ]
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
+          key=TRUE,margins=c(10,10))
+dev.off()
+
+#pE v male
+par(mar=c(4.1,4.1,4.1,4.1))
+png(file="heatmap_TCO_pE_v_male.png",height=2800,width=2800)
+selected  <- rownames(de_table2[de_table2$FDR<0.01,])
+esetSel <- dp_eset[selected, ]
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE,scale="row", density.info="none",trace="none",
+          key=TRUE,margins=c(10,10))
+dev.off()
+
+#asexuals vs pE
+par(mar=c(4.1,4.1,4.1,4.1))
+png(file="heatmap_TCO_matfem_v_pE.png",height=2800,width=2800)
+selected  <- rownames(de_table3[de_table3$FDR<0.01,])
 print(head(selected))
 esetSel <- dp_eset[selected, ]
-#heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE, scale="row", density.info="none",trace="none",
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE, scale="row", density.info="none",trace="none",
 #          key=TRUE, margins=c(10,10))
 #dev.off()
 
 #meiosis genes
 par(mar=c(4.1,4.1,4.1,4.1))
 png(file="heatmap_TCO_meiosis.png",height=2800,width=2800)
-meiosis_rows <- match(rownames(meiosis_table), rownames(top_table_e))
+meiosis_rows <- match(rownames(meiosis_table), rownames(de_table1))
 length(meiosis_rows)
 head(meiosis_rows)
 meiosis_rows <- na.omit(meiosis_rows)
-selected  <- rownames(top_table_e[meiosis_rows])
+selected  <- rownames(de_data1[meiosis_rows])
 esetSel <- dp_eset[selected, ]
-#heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE, scale="row", density.info="none",trace="none",
-#          key=TRUE, margins=c(10,10))
-#dev.off()
+heatmap.2(exprs(esetSel), symm=FALSE,symkey=FALSE, scale="row", density.info="none",trace="none",
+          key=TRUE, margins=c(10,10))
+dev.off()
 
 #gustatory receptors
 #par(mar=c(4.1,4.1,4.1,4.1))
