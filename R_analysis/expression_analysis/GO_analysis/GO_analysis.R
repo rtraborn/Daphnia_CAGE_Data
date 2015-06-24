a30216e35e5843d9f8a494173da7d998593debda
Taylor Raborn <rtraborn@indiana.edu>
2015-06-24 00:28:20 -0400

    Updated script to generate top 25 hits sorted by p-value in results tables

--- R_analysis/expression_analysis/GO_analysis/GO_analysis.R
+++ R_analysis/expression_analysis/GO_analysis/GO_analysis.R
@@ -169,7 +169,7 @@ resultKS.elim <- runTest(dpGOdata, algorithm = "elim", statistic = "ks")
 
 allRes <- GenTable(dpGOdata, classicFisher = resultFisher,
 classicKS = resultKS, elimKS = resultKS.elim,
- orderBy = "elimKS", ranksOf = "classicFisher", topNodes = 5)
+ orderBy = "elimKS", ranksOf = "classicFisher", topNodes = 25)
 
 printGraph(dpGOdata, resultKS, firstSigNodes = 5, useInfo = "all", pdfSW = TRUE,fn.prefix="MfvpE_upreg_tGO_BP")
 write.table(allRes,file="upreg_matfem_v_pE_GO_fisher_BP.txt",col.names=TRUE,row.names=FALSE,quote=TRUE,sep="\t")
