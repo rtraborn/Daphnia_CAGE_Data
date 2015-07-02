e5c25fef0cd9193ff044982a4be6f241dd44611d
Taylor Raborn <rtraborn@indiana.edu>
2015-07-02 12:32:25 -0400

    Added new version of TSS coverage plot

--- tss_coverage_plot/read_coverage_plot.R
+++ tss_coverage_plot/read_coverage_plot.R
@@ -1,12 +1,18 @@
 #written on April 8, 2015
 
 library("ggplot2")
-library("reshape2")
 
 #importing the coverage data
-total_cov <- read.table(file="total_read_coverage.txt",header=TRUE)
+total_cov <- read.table(file="total_read_coverage_i.txt",header=TRUE)
+total_cov <- as.data.frame(total_cov)
+total_cov$sample <- "total"
+names(total_cov) <- c("coverage_fraction","region","sample")
+
+#setting the colors (4)
+mycols <- c('#FFFD00', '#97CB00', '#3168FF', '#FF0200')
 
 #plotting the figure
-png("TSS_coverage_plot.png",width=960,height=600)
-ggplot(total_cov, aes(x = stage, y = fraction_of_reads,fill=region))+geom_bar(width=0.60,stat='identity')+coord_flip()
+png("TSS_coverage_plot.png",width=600,height=100)
+ggplot(total_cov, aes(y=coverage_fraction,x=sample,fill=region))+geom_bar(width=0.40,stat='identity')+coord_flip()
 dev.off()
+
