278736fe17b3bb3140833d1a6c73ad901db8ef52
rtraborn <rtraborn@indiana.edu>
2015-09-22 13:02:49 -0400

    Created VTG figures with geom_area()

--- R_analysis/CAGE_comparison_plot_VTG.R
+++ R_analysis/CAGE_comparison_plot_VTG.R
@@ -66,7 +66,7 @@ gr_gene2 <- gr_gene[gr_gene$name %in% "CDS"]
 
 gr_gene2
 
-plot <- autoplot(Dp_align, which = coord_region, method="raw",geom="line", color="#CC79A7",stat="coverage")
+plot <- autoplot(Dp_align, which = coord_region, method="raw",geom="area", color="#CC79A7", coverage.fill="#CC79A7", stat="coverage")
 
 is(plot)
 
@@ -121,7 +121,7 @@ Dp_align_p <- as(myStrand, "GAlignments")
 
 is(Dp_align_p)
 
-plot_peaked <- autoplot(Dp_align_p,which=coord_region,method="raw",geom="line",color="darkgreen",stat="coverage")
+plot_peaked <- autoplot(Dp_align_p,which=coord_region,method="raw",geom="area",color="darkgreen",coverage.fill="darkgreen",stat="coverage")
 
 p1_p <- plot_peaked + coord_cartesian(xlim = c(151539,157785)) + theme_bw()
 
@@ -172,7 +172,7 @@ Dp_align_p <- as(myStrand, "GAlignments")
 
 is(Dp_align_p)
 
-plot_peaked <- autoplot(Dp_align_p,which=coord_region,method="raw",geom="line",color="purple",stat="coverage")
+plot_peaked <- autoplot(Dp_align_p,which=coord_region,method="raw",geom="area",color="purple",coverage.fill="purple",stat="coverage")
 
 p1_p <- plot_peaked + coord_cartesian(xlim = c(151539,157785)) + theme_bw()
 
