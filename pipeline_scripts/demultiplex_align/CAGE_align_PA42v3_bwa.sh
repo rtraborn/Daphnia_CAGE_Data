6cb55f2a90d89ddd0546f2913f80c751f1ad20d1
rtraborn <rtraborn@indiana.edu>
2016-06-21 19:02:26 -0400

    Added alignment script for PA42v3

--- pipeline_scripts/demultiplex_align/CAGE_align_PA42v3_bwa.sh
+++ pipeline_scripts/demultiplex_align/CAGE_align_PA42v3_bwa.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-WD1=/scratch/rtraborn/Daphnia/PA42_genome
-GENOME=Daphnia_pulex_PA42_v3.0.fasta
-WD2=/scratch/rtraborn/Daphnia/CAGE_reads/demultiplexed_fastqs
+GENOME=/scratch/rtraborn/Daphnia/PA42_genome/Daphnia_pulex_PA42_v3.0.fasta
+WD1=/scratch/rtraborn/Daphnia/CAGE_reads/demultiplexed_fastqs
 DEST_DIR=/scratch/rtraborn/Daphnia/CAGE_reads/aligned_bams
-
-cd $WD1
 
 echo "Indexing the genome"
 
 bwa index $GENOME
 
-cd $WD2
+cd $WD1
 
 echo "Aligning the CAGE reads to PA42v3"
 
